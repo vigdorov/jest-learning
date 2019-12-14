b1eab2ed7c0425dcc2c72f4a76f3697ffa3b3de9
vigdorov <vigdorov@yandex-team.ru>
2019-12-14 20:16:54 +0300

    Написал основные тесты для простых функций

--- src/__test__/first.test.js
+++ src/__test__/first.test.js
@@ -1,4 +1,20 @@
-const {summaryTwoNumbers, calculateTwoNumbers} = require('../functions/first');
+const {summaryTwoNumbers, calculateTwoNumbers, getUsers} = require('../functions/first');
+
+const testAsyncData = {
+  data: [
+    {id: 7, email: "michael.lawson@reqres.in", first_name: "Michael", last_name: "Lawson", avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"},
+    {id: 8, email: "lindsay.ferguson@reqres.in", first_name: "Lindsay", last_name: "Ferguson", avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"},
+    {id: 9, email: "tobias.funke@reqres.in", first_name: "Tobias", last_name: "Funke", avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"},
+    {id: 10, email: "byron.fields@reqres.in", first_name: "Byron", last_name: "Fields", avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"},
+    {id: 11, email: "george.edwards@reqres.in", first_name: "George", last_name: "Edwards", avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"},
+    {id: 12, email: "rachel.howell@reqres.in", first_name: "Rachel", last_name: "Howell", avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"}
+  ],
+  length: 6,
+  page: 2,
+  per_page: 6,
+  total: 12,
+  total_pages: 2,
+};
 
 describe('Первые тесты', () => {
     it('Функция summaryTwoNumbers', () => {
@@ -36,5 +52,52 @@ describe('Первые тесты', () => {
       });
 
       expect(testResult).toEqual(result);
-    })
+    });
+
+    it('two plus two', () => {
+      const value = 2 + 2;
+
+      expect(value).not.toBe(5);
+      expect(value).not.toBeNull();
+      expect(value).toBeDefined();
+      expect(value).not.toBeUndefined();
+      expect(value).not.toBeFalsy();
+      expect(value).toBeTruthy();
+    });
+
+    it('is string test', () => {
+      const str = 'Hello world!';
+
+      expect(str).toMatch(/ello/);
+      expect(str).not.toMatch(/jake/);
+    });
+
+    it('Проверка массива', () => {
+      const array = ['red', 'blue', 'yellow', 'black'];
+
+      expect(array).toContain('red');
+      expect(array).not.toContain('brown');
+    });
+
+    it('Проверка ошибок', () => {
+      function compileCode () {
+        throw new Error('Use a normal pc');
+      }
+
+      expect(compileCode).toThrow();
+      expect(compileCode).toThrow(Error);
+      expect(compileCode).toThrow('Use a normal pc');
+      expect(compileCode).toThrow(/pc/);
+    });
+
+    it('Тест асинхронных функций с использованием await', async () => {
+      const users = await getUsers();
+      expect(users).toEqual(testAsyncData);
+    });
+
+    it('Тест асинхронных функций с использованием обещаний', () => {
+      return getUsers().then(res => {
+        expect(res).toEqual(testAsyncData);
+      });
+    });
 });
